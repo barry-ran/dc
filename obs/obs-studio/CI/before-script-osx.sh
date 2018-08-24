09faf1655a8d5c21b7de21ca5ff41cc8dca3ac19
miaozw <miaozw@yunxuetang.cn>
2018-08-24 15:19:19 +0800

    增加obs源码v22.0.1  1 20180824 e4c3516af6ed6e174e70b7e3208f2e860ad908c

---
+++ obs/obs-studio/CI/before-script-osx.sh
@@ -0,0 +1,13 @@
+# Make sure ccache is found
+export PATH=/usr/local/opt/ccache/libexec:$PATH
+
+git fetch --tags
+
+mkdir build
+cd build
+cmake -DENABLE_SPARKLE_UPDATER=ON \
+-DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 \
+-DDepsPath=/tmp/obsdeps \
+-DVLCPath=$PWD/../../vlc-master \
+-DBUILD_BROWSER=ON \
+-DCEF_ROOT_DIR=$PWD/../../cef_binary_${CEF_BUILD_VERSION}_macosx64 ..
